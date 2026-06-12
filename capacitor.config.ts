b278c0de861f523421b141a4eba3e3d800febe0a
Julian Radevych <radevich7@gmail.com>
2026-06-11 18:21:34 -0600

    Setup ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.activityplatform.app',
+  appName: 'activity-platform-client',
+  webDir: 'dist/activity-platform-client/browser'
+};
+
+export default config;
